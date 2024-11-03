fd0518e7e918b8218e6fc4a00f1890387488dccb
Psykedelius <psykedelius@gmail.com>
2024-11-04 00:18:45 +0100

    home page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/", "Reserver", "/pages/reserver.html"),
+    new Route("/index.html", "Accueil", "/pages/home.html"),];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Bistro breton";
