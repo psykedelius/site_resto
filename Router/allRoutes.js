15f6dcaac1331fea591db4a25f263b3836fc8282
Psykedelius <psykedelius@gmail.com>
2024-11-04 23:27:24 +0100

    add page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,9 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/", "Reserver", "/pages/reserver.html"),
-    new Route("/index.html", "Accueil", "/pages/home.html"),];
+    new Route("/"       , "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/reserver.html", "Reserver", "/pages/reserver.html"),
+    new Route("/index.html", "Accueil", "/pages/home.html")
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Bistro breton";
