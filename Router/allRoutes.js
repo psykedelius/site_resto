352939d3707faead0aa95cdf0b58e5faa84fde56
Psykedelius <psykedelius@gmail.com>
2024-11-07 23:53:17 +0100

    ajout modals edition galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,9 @@ export const allRoutes = [
     new Route("/"       , "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/reserver", "Reserver", "/pages/reserver.html"),
-    new Route("/index.html", "Accueil", "/pages/home.html")
+    new Route("/index.html", "Accueil", "/pages/home.html"),
+    new Route("/signin", "Connection", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Bistro breton";
